50cfdbe75da0e08ff0964a8dffddc69780e72915
Kimiko Kano <kimiko.kano@crownpeak.com>
2017-12-11 15:33:20 -0700

    Add deleteAssets function

--- frontend/react-ui/src/services/APIService.js
+++ frontend/react-ui/src/services/APIService.js
@@ -8,8 +8,16 @@ export const getAssets = () => {
     .then(response => { return response }));
 }
 
-export const putAssets = () =>  {
+export const putAssets = (data) =>  {
   fetch(`${this.getBaseUrl}/Assets`, {
-    method: 'put'
+    method: 'put',
+    body: data
   }).then(response => { return response.json() });
+}
+
+export const deleteAssets = (assetId) => {
+  fetch(`${this.getBaseUrl}/Assets/${assetId}`, {
+    method: 'delete',
+  }).then(response => response.json()
+    .then(response => { return response }));
 }
