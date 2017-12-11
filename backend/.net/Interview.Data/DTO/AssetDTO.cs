19b5d4a801509ae60833a7c49aaf1aac35905f8c
JT Bruch <jt.bruch@crownpeak.com>
2017-12-11 14:43:09 -0700

    Add missing file for compilation.

---
+++ backend/.net/Interview.Data/DTO/AssetDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.Data.DTO
+{
+  public class AssetDTO
+  {
+    public string Name { get; set; }
+    public int? Id { get; set; }
+  }
+}
